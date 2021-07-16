7bb7cc1019edf511966057ab16f0512315478d4c
Mukuna <mukuna@lightswitch.solutions>
2021-07-16 10:43:30 +0200

    WIP

---
+++ lib/models/user.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String username;
+  final String password;
+  final String sessionId;
+
+  User({
+    this.username,
+    this.password,
+    this.sessionId,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      username: json["username"],
+      password: json["password"],
+      sessionId: json["session_id"],
+    );
+  }
+
+  @override
+  List<Object> get props => [username, password, sessionId];
+}
